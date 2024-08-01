6d4df1d34734adc1e1996fb880ae1386475dfb7d
Dani <rodriguez.cesar725@gmail.com>
2024-08-01 17:14:25 -0400

    Inicialización de la aplicación y creación de controladores y rutas

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'projects', to: 'pages#projects'
+  get 'contact', to: 'pages#contact'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
